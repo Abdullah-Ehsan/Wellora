2457b26a3e905311d31de838bccccdcb8e1a1042
Abdullah <abdullahehsan001@gmail.com>
2026-01-04 00:16:38 +0500

    somw changes

--- obj/Debug/net8.0/Wellora.AssemblyInfo.cs
+++ obj/Debug/net8.0/Wellora.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("Wellora")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+e3f671a8225e6e3235ccfa988229f4614874b213")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+a98900e18bc7cf129e665e73832f5abdb8c012b6")]
 [assembly: System.Reflection.AssemblyProductAttribute("Wellora")]
 [assembly: System.Reflection.AssemblyTitleAttribute("Wellora")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
