8d9f966bb32311c40365728c1ee1de5cbfb1bfd4
Abdullah <abdullahehsan001@gmail.com>
2026-05-14 22:15:29 +0500

    Clean up the structer and addd folder

--- Areas/Admin/Controllers/AdminController.cs
+++ Areas/Admin/Controllers/AdminController.cs
@@ -2,9 +2,15 @@
 
 namespace Wellora.Areas.Admin.Controllers
 {
+    [Area("Admin")]
     public class AdminController : Controller
     {
         public IActionResult Index()
+        {
+            return View();
+        }
+
+        public IActionResult AdminLogin()
         {
             return View();
         }
