41531a28691fb11f0c7a3a1c0a2fba05c888f066
Abdullah <abdullahehsan001@gmail.com>
2026-06-05 16:42:41 +0500

    added some changes

---
+++ ViewModels/ContactViewModel.cs
@@ -0,0 +1,19 @@
+﻿namespace Wellora.ViewModels
+{
+    public class ContactViewModel
+    {
+        public string Phone { get; set; } = "+92 300 1234567";
+        public string Email { get; set; } = "info@example.com";
+        public string Address { get; set; } = "Govt Jinnah Islamia College, Sialkot";
+
+        public ContactFormModel Form { get; set; } = new ContactFormModel();
+    }
+
+    public class ContactFormModel
+    {
+        public string FirstName { get; set; }
+        public string LastName { get; set; }
+        public string Email { get; set; }
+        public string Message { get; set; }
+    }
+}
