2ce03e54b42b9ab7f7b6c024ef3109d872d1a0d7
Abdullah <abdullahehsan001@gmail.com>
2026-06-16 18:29:51 +0500

    added these pages & controller for the extra features

---
+++ Areas/Doctor/Controllers/PatientGetDiagnoseController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Wellora.Areas.Doctor.Controllers
+{
+    [Area ("Doctor")]
+    [Authorize (Roles = "doctor")]
+    public class PatientGetDiagnoseController : Controller
+    {
+        public IActionResult PatientDiagnoses()
+        {
+            return View();
+        }
+    }
+}
---
+++ Areas/Doctor/Controllers/PatientGetReportedController.cs
@@ -0,0 +1,15 @@
+﻿using Microsoft.AspNetCore.Authorization;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Wellora.Areas.Doctor.Controllers
+{
+    [Area("Doctor")]
+    [Authorize(Roles = "doctor")]
+    public class PatientGetReportedController : Controller
+    {
+        public IActionResult PatientReported()
+        {
+            return View();
+        }
+    }
+}
