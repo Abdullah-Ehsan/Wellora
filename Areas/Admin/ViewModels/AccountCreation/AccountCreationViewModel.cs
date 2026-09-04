e5ae123ac577ca4f6d7a60de2a65b18f10d74965
Abdullah <abdullahehsan001@gmail.com>
2026-09-04 22:49:08 +0500

    added viewmodel for the account creation

---
+++ Areas/Admin/ViewModels/AccountCreation/AccountCreationViewModel.cs
@@ -0,0 +1,11 @@
+﻿namespace Wellora.Areas.Admin.ViewModels.AccountCreation
+{
+    public class AccountCreationViewModel
+    {
+        public CreateUserAccountViewModel UserAccount { get; set; }
+            = new CreateUserAccountViewModel();
+
+        public CreateAdminAccountViewModel AdminAccount { get; set; }
+            = new CreateAdminAccountViewModel();
+    }
+}
---
+++ Areas/Admin/ViewModels/AccountCreation/CreateAdminAccountViewModel.cs
@@ -0,0 +1,37 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace Wellora.Areas.Admin.ViewModels.AccountCreation
+{
+    public class CreateAdminAccountViewModel
+    {
+        [Required(ErrorMessage = "First name is required.")]
+        [Display(Name = "First Name")]
+        public string? FirstName { get; set; }
+
+        [Required(ErrorMessage = "Last name is required.")]
+        [Display(Name = "Last Name")]
+        public string? LastName { get; set; }
+
+        [Required(ErrorMessage = "Username is required.")]
+        [Display(Name = "Username")]
+        public string? Username { get; set; }
+
+        [Required(ErrorMessage = "Email is required.")]
+        [EmailAddress(ErrorMessage = "Please enter a valid email address.")]
+        [Display(Name = "Email")]
+        public string? Email { get; set; }
+
+        [Required(ErrorMessage = "Password is required.")]
+        [DataType(DataType.Password)]
+        [RegularExpression(
+            @"^(?=.*[A-Z])(?=.*\d)(?=.*[!@#$%^&*]).{8,}$",
+            ErrorMessage = "Password must be at least 8 characters and include one uppercase letter, one number, and one special character."
+        )]
+        [Display(Name = "Password")]
+        public string? Password { get; set; }
+
+        [Required(ErrorMessage = "Please select admin seniority.")]
+        [Display(Name = "Seniority")]
+        public string Seniority { get; set; } = "junior";
+    }
+}
