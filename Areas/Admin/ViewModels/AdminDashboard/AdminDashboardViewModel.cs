1658fc65a19d55ca32c6b5da39812e29fccaa7c7
Abdullah <abdullahehsan001@gmail.com>
2026-09-04 22:51:00 +0500

    added  doctor dash

---
+++ Areas/Admin/ViewModels/AdminDashboard/AdminDashboardViewModel.cs
@@ -0,0 +1,42 @@
+﻿using System;
+
+namespace Wellora.Areas.Admin.ViewModels.AdminDashboard
+{
+    public class AdminDashboardViewModel
+    {
+        // Basic IDs
+        public int AdminId { get; set; }
+        public int UserId { get; set; }
+
+        // User info
+        public string FullName { get; set; } = "";
+        public string Username { get; set; } = "";
+        public string Email { get; set; } = "";
+
+        // Admin info
+        public string? ProfilePicture { get; set; }
+        public string? Gender { get; set; }
+        public string? ContactNumber { get; set; }
+        public string? Address { get; set; }
+
+        public DateTime? DateOfBirth { get; set; }
+
+        public int? Age
+        {
+            get
+            {
+                if (!DateOfBirth.HasValue) return null;
+
+                var today = DateTime.Today;
+                var age = today.Year - DateOfBirth.Value.Year;
+
+                if (DateOfBirth.Value.Date > today.AddYears(-age)) age--;
+
+                return age;
+            }
+        }
+
+        public string? AdminType { get; set; }
+        public string? Status { get; set; }
+    }
+}
